e334d81a5607a790990e8cfa413a1f5acedb3933
theo <tmarty@teclib.com>
2020-01-02 14:19:10 +0100

    Add nextcloud docker-compose

--- start_all.sh
+++ start_all.sh
@@ -2,12 +2,13 @@
 docker network create web
 docker-compose \
 	-f docker-compose-traefik.yml \
-        -f docker-compose-db.yml \
+	-f docker-compose-db.yml \
 	-f docker-compose-wordpress.yml \
         pull
 
 docker-compose \
-        -f docker-compose-traefik.yml \
-        -f docker-compose-db.yml \
+    -f docker-compose-traefik.yml \
+    -f docker-compose-db.yml \
 	-f docker-compose-wordpress.yml \
+	-f docker-compose-nextcloud.yml \
         up
